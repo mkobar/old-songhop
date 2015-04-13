e9bf2c22228e5a52e191922829a731b4f10576fa
mike kobar <mike@exlorev.com>
2015-04-13 12:27:31 -0400

    auth bug fixes

--- code/www/js/controllers.js
+++ code/www/js/controllers.js
@@ -2,12 +2,49 @@ angular.module('songhop.controllers', ['ionic', 'songhop.services'])
 
 
 /*
+Controller for the splash page
+*/
+.controller('SplashCtrl', function($scope, $state, User) {
+   // attempt to signup/login via User.auth
+   $scope.submitForm = function(username, signingUp) {
+      User.auth(username, signingUp).then(function(){
+         // session is now set so redirect to discover page
+         $state.go('tab.discover');
+      }, function() {
+         // error handling
+         alert('Hmm... try another username');
+      });
+   }
+
+})
+
+/*
 Controller for the discover page
 */
-.controller('DiscoverCtrl', function($scope,$timeout, User, Recommendations) {
-      Recommendations.getNextSongs()
+.controller('DiscoverCtrl', function($scope, $ionicLoading, $timeout, User, Recommendations) {
+
+    // helper functions for loading
+    var showLoading = function() {
+       $ionicLoading.show({
+          template: '<i class="ion-loading-c"></i>',
+          noBackdrop: true
+       });
+    }
+
+    var hideLoading = function() {
+       $ionicLoading.hide();
+    }
+
+    // set loading to true the first time we load data from the server
+    showLoading();
+
+    // get our first songs
+    Recommendations.getNextSongs()
     .then(function(){
       $scope.currentSong = Recommendations.queue[0];
+    }).then(function(){
+       hideLoading();
+       $scope.currentSong.loaded = true;
     });
    
     /*$scope.songs = [
@@ -33,21 +70,24 @@ Controller for the discover page
     
     $scope.currentSong = angular.copy($scope.songs[0]);
     */
-    
+   
+    // fired when we favorite / skip a song 
     $scope.sendFeedback = function (bool){
-          // first, add to favorites if they favorited
-    if (bool) User.addSongToFavorites($scope.currentSong);
-        $scope.currentSong.rated = bool;
-        $scope.currentSong.hide = true;
-        
-        // prepare the next song
-    Recommendations.nextSong();
+       // first, add to favorites if they favorited
+       if (bool) User.addSongToFavorites($scope.currentSong);
 
-    $timeout(function() {
-      // $timeout to allow animation to complete
-      $scope.currentSong = Recommendations.queue[0];
-    }, 250);
-    }
+       // set variables for the correct animation seq
+       $scope.currentSong.rated = bool;
+       $scope.currentSong.hide = true;
+        
+       // prepare the next song
+       Recommendations.nextSong();
+
+       $timeout(function() {
+          // $timeout to allow animation to complete
+          $scope.currentSong = Recommendations.queue[0];
+       }, 250);
+    };
     
     // used for retrieving the next album image.
   // if there isn't an album image available next, return empty string.
@@ -57,7 +97,7 @@ Controller for the discover page
     }
 
     return '';
-  }
+  };
 
 })
 
@@ -65,12 +105,18 @@ Controller for the discover page
 /*
 Controller for the favorites page
 */
-.controller('FavoritesCtrl', function($scope, User) {
+.controller('FavoritesCtrl', function($scope, User, $window) {
     $scope.favorites = User.favorites;
+    $scope.username = User.username;
+
+    // do this when favorite song is selected
+    $scope.openSong = function(song) {
+       $window.open(song.open_url, "_system");  // open in browser
+    };
     
      $scope.removeSong = function(song, index) {
     User.removeSongFromFavorites(song, index);
-  }
+  };
     
 
 })
@@ -79,6 +125,13 @@ Controller for the favorites page
 /*
 Controller for our tab bar
 */
-.controller('TabsCtrl', function($scope) {
+.controller('TabsCtrl', function($scope, User, $window) {
+    $scope.logout = function() {
+       User.destroySession();
+
+       // instead of using $state.go, we're going to redirect.
+       // reason: we need to ensure views aren't cached.
+       $window.location.href = 'index.html';
+    };
 
 });
